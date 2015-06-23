b7a4c154c63f36d29474d7fa152fb12143008207
beyondliangcai <beyondliangcai@163.com>
2015-06-23 17:08:22 +0800

    去掉注释

--- src/com/example/appointphoto/MainActivity.java
+++ src/com/example/appointphoto/MainActivity.java
@@ -12,7 +12,7 @@ import android.view.ViewGroup;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
-//woca
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

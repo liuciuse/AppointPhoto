a46f87946fb3d7a01fbe690deebb264abf199f02
liuciuse <leikaikailkk@163.com>
2015-06-26 12:52:10 +0800

    添加刷新

--- src/com/appointphoto/activity/HomeFragment.java
+++ src/com/appointphoto/activity/HomeFragment.java
@@ -1,41 +1,112 @@
 package com.appointphoto.activity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.HorizontalScrollView;
 
 import com.example.appointphoto.R;
+import com.handmark.pulltorefresh.library.ILoadingLayout;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.Mode;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.special.ResideMenu.ResideMenu;
 
 /**
- * User: special
- * Date: 13-12-22
- * Time: 涓嬪崍1:33
- * Mail: specialcyci@gmail.com
+ * User: special Date: 13-12-22 Time: 涓嬪崍1:33 Mail: specialcyci@gmail.com
  */
 public class HomeFragment extends Fragment {
 
-    private View parentView;
-    private ResideMenu resideMenu;
+	private View parentView;// 整个界面
+	private ResideMenu resideMenu;// 侧栏
+	private PullToRefreshListView mPullRefreshListView;// 可刷新listview
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        parentView = inflater.inflate(R.layout.homelistview, container, false);
-        setUpViews();
-        return parentView;
-    }
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		parentView = inflater.inflate(R.layout.homelistview, container, false);
+		setUpViews();
+		return parentView;
+	}
 
-    private void setUpViews() {
-        MenuActivity parentActivity = (MenuActivity) getActivity();
-        resideMenu = parentActivity.getResideMenu();
+	private void setUpViews() {
+		MenuActivity parentActivity = (MenuActivity) getActivity();
+		resideMenu = parentActivity.getResideMenu();
 
-        // add gesture operation's ignored views 水平滚动条不触发菜单手势
-        HorizontalScrollView ignored_view = (HorizontalScrollView) parentView.findViewById(R.id.header_ScrollView);
-        resideMenu.addIgnoredView(ignored_view);
-    }
+		// add gesture operation's ignored views 水平滚动条不触发菜单手势
+		HorizontalScrollView ignored_view = (HorizontalScrollView) parentView
+				.findViewById(R.id.header_ScrollView);
+		resideMenu.addIgnoredView(ignored_view);
 
+		// 初始化刷新
+		mPullRefreshListView = (PullToRefreshListView) parentView
+				.findViewById(R.id.pull_refresh_list);
+		// 设置两端刷新
+		mPullRefreshListView.setMode(Mode.BOTH);
+		ILoadingLayout startLabels = mPullRefreshListView.getLoadingLayoutProxy(true, false);
+		startLabels.setPullLabel("释放刷新...");// 刚下拉时，显示的提示
+		startLabels.setRefreshingLabel("正在刷新...");// 刷新时
+		startLabels.setReleaseLabel("释放刷新...");// 下来达到一定距离时，显示的提示
+
+		ILoadingLayout endLabels = mPullRefreshListView.getLoadingLayoutProxy(false, true);
+		endLabels.setPullLabel("加载更多...");// 刚下拉时，显示的提示
+		endLabels.setRefreshingLabel("正在载入...");// 刷新时
+		endLabels.setReleaseLabel("加载更多...");// 下来达到一定距离时，显示的提示
+		mPullRefreshListView
+				.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2() {
+
+					@Override
+					public void onPullDownToRefresh(
+							PullToRefreshBase refreshView) {
+						new PullRefresh().execute();
+					}
+
+					@Override
+					public void onPullUpToRefresh(PullToRefreshBase refreshView) {
+						new PullupGetMore().execute();
+					}
+
+				});
+	}
+
+	// 下拉刷新
+	private class PullRefresh extends AsyncTask<Void, Void, Void> {
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+				Thread.sleep(1000);
+				// 从数据库获取最新数据
+
+			} catch (InterruptedException e) {
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			mPullRefreshListView.onRefreshComplete();
+		}
+	}
+
+	// 上拉加载更多
+	private class PullupGetMore extends AsyncTask<Void, Void, Void> {
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+				Thread.sleep(1000);
+				// 从数据库加载更多数据
+
+			} catch (InterruptedException e) {
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			mPullRefreshListView.onRefreshComplete();
+		}
+	}
 }
